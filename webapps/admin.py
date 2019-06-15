fbea0b0235e7c884a06b94a96709de65ac5f0c47
jeff lu <jeff.lu0419@gmail.com>
2019-06-15 14:57:41 -0700

    Add db management in admin

--- webapps/admin.py
+++ webapps/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from webapps.models import *
+
+admin.site.register(instrument_files)
+admin.site.register(def_files)
+admin.site.register(setting_files)
+admin.site.register(signal_def_files)
+admin.site.register(seq_defs)
+admin.site.register(time_scales)
+admin.site.register(date_formats)
+admin.site.register(resolution)
+admin.site.register(instruments)
+admin.site.register(populations)
+
 
 # Register your models here.
