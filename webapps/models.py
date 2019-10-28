007534d6999d5073cd07aa1d9b034d94a4adfaf3
jeff lu <jeff.lu0419@gmail.com>
2019-10-27 18:50:57 -0700

    Remove timescale and date format tables Add start time/ end time to the instrument table

--- webapps/models.py
+++ webapps/models.py
@@ -1,34 +1,43 @@
 from django.db import models
 
 class Seq_def(models.Model):
-  name = models.CharField(max_length=30, primary_key=True)
+  name = models.CharField(max_length=30)
   description = models.CharField(max_length=60)
-
-class Time_scale(models.Model):
-  name = models.CharField(max_length=30, primary_key=True)
-
-class Date_format(models.Model):
-  name = models.CharField(max_length=10, primary_key=True)
-  format = models.CharField(max_length=30)
 
 # Default resolutions
 class Resolution(models.Model):
-  value = models.CharField(max_length=10, primary_key=True)
+  value = models.CharField(max_length=10)
 
 class Instrument(models.Model):
-  name = models.CharField(max_length=30, primary_key=True)
+  name = models.CharField(max_length=30)
   history_data_file = models.CharField(max_length=30)
   instrument_def_file = models.CharField(max_length=30)
-  time_scale = models.ForeignKey(Time_scale, on_delete=models.CASCADE) # TODO change to ENUM
-  date_format = models.ForeignKey(Date_format, on_delete=models.CASCADE) # TODO change to ENUM
-  min_movement = models.CharField(max_length=30)
-  price_scale = models.CharField(max_length=30)
-  big_point_value = models.CharField(max_length=30)
+  time_scale = models.CharField(max_length=5)
+  date_format = models.CharField(max_length=10)
+  min_movement = models.CharField(max_length=10)
+  price_scale = models.CharField(max_length=10)
+  big_point_value = models.CharField(max_length=10)
   query = models.CharField(max_length=200)
+  start_date = models.DateField()
+  end_date = models.DateField()
   created_at = models.DateTimeField(auto_now_add=True)
 
+
+
+  def dump(self):
+    print(self.name)
+    print(self.history_data_file)
+    print(self.instrument_def_file)
+    print(self.time_scale)
+    print(self.date_format)
+    print(self.min_movement)
+    print(self.price_scale)
+    print(self.big_point_value)
+    print(self.query)
+
+
 class Population(models.Model):
-  name = models.CharField(max_length=30, primary_key=True)
+  name = models.CharField(max_length=30)
   instrument = models.ForeignKey(Instrument, on_delete=models.CASCADE)
   simulation_setting_file = models.CharField(max_length=30)
   signal_def_file = models.CharField(max_length=30)
